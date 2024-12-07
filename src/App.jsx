0da6e03c09b9583497852d17f53bac267bae33e9
jagoncalvesrodrigues <jagoncalvesrodrigues@gmail.com>
2024-12-07 11:37:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ const App = () => {
 	return (
 		<div>
 			<GlobalStyles />
-			<h1>Curso de React</h1>
+			<h1>10,000+ of our users love our products.</h1>
+			<p>We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
+			<Rates text="Rated 5 Stars in Reviews"/>
+			<Rates text="Rated 5 Stars in Report Guru"/>
+			<Rates text="Rated 5 Stars in BestTech"/>
+			<Opinions>
+				<UserOpinion/>
+				<UserOpinion/>
+				<UserOpinion/>
+			</Opinions>
+			{/* <h1>Curso de React</h1>
 			<Button color='steelblue'>Click Me!</Button>
-			<Button color='#f0f'>Click Me!</Button>
+			<Button color='#f0f'>Click Me!</Button> */}
 		</div>
 	);
 };
